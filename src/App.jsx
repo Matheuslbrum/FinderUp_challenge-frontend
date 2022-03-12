388cf7873ed4c8ef6865d423c67d5ee485856ffd
Matheuslbrum <matheusdelimabrum@gmail.com>
2022-03-12 03:31:06 -0300

    doing query by query params

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 function App() {
+  const [materials, setMaterials] = useState([]);
+
+  useEffect(() => {
+    api.get('rawMaterials', {
+      params: {
+        name: 'teste',
+      },
+    }).then(({ data }) => {
+      setMaterials(data.materials);
+    }).catch((error) => console.log(error));
+  }, []);
+
   return (
-    <p>hello world</p>
+    <div>
+      <ul>
+        {
+         materials.map((material) => (
+           <li key={material.id}>
+             {material.id}
+             {material.name}
+             {material.quantity}
+             {material.user}
+             {material.create_at}
+             {material.update_at}
+           </li>
+))
+      }
+      </ul>
+    </div>
   );
 }
 
